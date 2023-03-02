7e090a9a443fec482d2d9fbe0a77e99aabcd7a41
JunTaeINC <juntea3523@naver.com>
2023-03-02 09:27:32 +0900

    5단계

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,13 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        List<명언모음집> 명언집 = new ArrayList<>();
         int count = 1;
         System.out.println("== 명언 앱 ==");
         while (true) {
@@ -17,13 +20,20 @@ public class Main {
                 String famousSaying = scanner.nextLine();
                 System.out.print("작가 : ");
                 String author = scanner.nextLine();
-                System.out.printf("%d번 명언이 등록되었습니다.\n",count);
+                System.out.printf("%d번 명언이 등록되었습니다.\n", count);
+                명언집.add(new 명언모음집(count, famousSaying, author));
                 count++;
+            } else if (input.equals("목록")) {
+                System.out.println("번호 / 작가 / 명언\n----------------------");
+                for (명언모음집 명언 : 명언집) {
+                    System.out.printf("%d / %s / %s\n", 명언.getId(), 명언.get작가(), 명언.get명언());
+                }
             }
         }
     }
 }
-class 명언모음집{
+
+class 명언모음집 {
     int id;
     String 명언;
     String 작가;
