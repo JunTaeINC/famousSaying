0acb7b50012565e93bd34bc171ba6cd3ec58988c
JunTaeINC <juntea3523@naver.com>
2023-03-02 09:19:09 +0900

    2단계

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,8 +9,13 @@ public class Main {
         while (true) {
             System.out.print("명언) ");
             String input = scanner.nextLine();
-            if (input.equals("종료")){
+            if (input.equals("종료")) {
                 break;
+            } else if (input.equals("등록")) {
+                System.out.print("명언 : ");
+                String famousSaying = scanner.nextLine();
+                System.out.print("작가 : ");
+                String author = scanner.nextLine();
             }
         }
     }
